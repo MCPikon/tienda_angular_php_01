5cb1cf60a5429ba6df14aff22cbeafc4d089d20f
Javier Picón <82212139+MCPikon@users.noreply.github.com>
2022-01-28 18:42:20 +0100

    version final

--- admin/gestionar_pedidos.php
+++ admin/gestionar_pedidos.php
@@ -1,6 +1,11 @@
 <?php
 require "../librerias_php/setup_redbean.php";
 
+if (isset($_GET["idBorrar"])) {
+    R::exec('DELETE FROM pedidos WHERE id = ?', [$_GET["idBorrar"]]);
+    R::exec('DELETE FROM productopedido WHERE id_pedido = ?', [$_GET["idBorrar"]]);
+}//fin if
+
 $sql = "SELECT p.id, v.titulo, v.precio, v.descripcion, v.estado, v.formato, v.editor, v.plataforma, p.nombre, p.direccion, p.tarjeta, pp.cantidad 
 FROM videojuegos AS v, pedidos AS p, productopedido AS pp WHERE p.id = pp.id_pedido AND v.id = pp.id_producto ORDER BY p.id DESC;";
 
